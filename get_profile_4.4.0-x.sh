0a7b0cced8a25fa4142f4ba6988bfa1ebc001816
hakawati <hakawati@naver.com>
2020-05-26 16:42:05 +0900

    Auto Profiling from ubuntu memory map

---
+++ get_profile_4.4.0-x.sh
@@ -0,0 +1,147 @@
+#!/bin/bash
+
+kflag=$(uname -r | sed 's/-/ /g' | awk '{print $1}')
+kflag_profile=$(echo $kflag | sed 's/\./_/g')
+vflag=$(uname -r | sed 's/-/ /g' | awk '{print $2}')
+home=$(pwd)
+
+avml_url="https://github.com/microsoft/avml/releases/download/v0.2.0/avml"
+elfvol_url="http://downloads.volatilityfoundation.org/releases/2.6/volatility_2.6_lin64_standalone.zip"
+volsrc_url="https://github.com/volatilityfoundation/volatility.git"
+
+if ! [[ -f "$home/crond" ]]; then
+  echo "@reboot $home/`basename "$0"` | tee $home/runlog.txt" > $home/crond
+  crontab -u $USER $home/crond
+fi
+
+if [[ "$(id -u)" == "0" ]]; then
+  echo -e 'no dot run on root'
+  exit 1
+fi
+
+if ! [[ -f "$home/password.txt" ]]; then
+  read -sp "[sudo] password for $USER: " password
+  echo $password > "$home/password.txt"
+else
+  password=$(cat "$home/password.txt")
+fi
+
+while true; do
+  check_ps=$(ps -ef | grep apt | grep -v 'grep' | awk '{print $2}')
+  if [[ -n "$check_ps" ]]; then
+    echo -e '\nstop apt process'
+    ps -ef | grep apt | grep -v 'grep'
+    echo $password | sudo -S pkill apt
+  else
+    break
+  fi
+done
+
+declare -a coms=( 'wget' 'git' 'zip' 'unzip' 'make' 'gcc' 'vim' 'dwarfdump' )
+
+echo $password | sudo -S apt update
+for com in ${coms[@]}; do
+  if [[ -z $(command -v $com) ]]; then
+    echo -e "----------------------------------------install $com"
+    echo $password | sudo -S apt -y install $com
+  fi
+done
+
+declare -a dirs=( 'tools' 'ubuntu' )
+
+for dir in ${dirs[@]}; do
+  if ! [[ -d "$home/$dir" ]]; then
+    mkdir -p "$home/$dir"
+  fi
+done
+
+if ! [[ -f "$home/avml" ]]; then
+  wget "$avml_url"
+  echo $password | sudo -S chmod +x $home/avml
+fi
+
+if ! [[ -f "$home/volatility" ]]; then
+  cd $home/tools
+  wget "$elfvol_url"
+  unzip volatility_2.6_lin64_standalone.zip
+  mv volatility_2.6_lin64_standalone/volatility_2.6_lin64_standalone $home/volatility
+  cd $home
+  echo $password | sudo -S chmod +x $home/volatility
+fi
+
+if ! [[ -d "$home/tools/volatility" ]]; then
+  cd $home/tools
+  git clone "$volsrc_url"
+  cd $home
+fi
+
+echo $password | sudo -S "$home/avml" "$home/$(uname -r).lime"
+while true; do
+  if [[ -f "$home/pflag.txt" ]]; then
+    voltest=$($home/volatility --plugins=ubuntu --profile=Linuxubuntu-$kflag_profile-$(cat "$home/pflag.txt")-genericx64 -f $(uname -r).lime linux_pslist | tail -n 1 | awk '{print $NF}')
+    error_message=$(echo -e $voltest | grep 'No suitable address space mapping found')
+  fi
+  if [[ -z "$voltest" || "$voltest" == '-' || "$voltest" == '0' || -n "$error_message" ]]; then
+    cd "$home/tools/volatility/tools/linux"
+    while true; do
+      if [[ -f "ubuntu-$(uname -r).zip" ]]; then
+        rm -f "ubuntu-$(uname -r).zip"
+      fi
+      make clean && make
+      echo $password | sudo -S zip ubuntu-$(uname -r).zip module.dwarf /boot/System.map-$(uname -r)
+      profile_test=$(wc -c module.dwarf | awk '{print $1}')
+      if [[ "$profile_test" != '0' ]]; then
+        break
+      fi
+    done
+    mv -f ubuntu-$(uname -r).zip $home/ubuntu/
+    cd $home
+    echo $vflag > "$home/pflag.txt"
+  else
+    rm -f $home/$(uname -r).lime
+    break
+  fi
+done
+
+version_file="$home/versions.txt"
+del_version_file="$home/del_versions.txt"
+if ! [[ -f "$version_file" ]]; then
+  echo $password | sudo -S apt-cache search linux-headers-$kflag | grep 'generic' | sed 's/-/ /g' | awk '{print $4}' | sort -n > "$version_file"
+fi
+
+declare -a versions=( $(cat $version_file) )
+declare -a delkernel=( $(cat $del_version_file) )
+declare -a delfiles=( 'password.txt' 'pflag.txt' 'versions.txt' 'del_versions.txt' 'crond' )
+
+if [[ ${#versions[@]} -ne 1 ]]; then
+  ins_all_header="linux-headers-$kflag-${versions[1]}"
+  ins_generic_header="linux-headers-$kflag-${versions[1]}-generic"
+  ins_generic_image="linux-image-$kflag-${versions[1]}-generic"
+  echo $password | sudo -S apt -y install "$ins_all_header" "$ins_generic_header" "$ins_generic_image"
+
+  if [[ ${#delkernel[@]} -ge 2 ]]; then
+    del_all_header="linux-headers-$kflag-${delkernel[1]}"
+    del_generic_header="linux-headers-$kflag-${delkernel[1]}-generic"
+    del_generic_image="linux-image-$kflag-${delkernel[1]}-generic"
+    echo $password | sudo -S apt -y autoremove "$del_all_header" "$del_generic_header" "$del_generic_image"
+    unset delkernel[1]
+    echo ${delkernel[@]} > "$del_version_file"
+  fi
+
+  echo ${versions[0]} >> "$del_version_file" 
+  unset versions[0]
+  echo ${versions[@]} > "$version_file"
+  echo $password | sudo -S reboot
+else
+  for delversion in ${delkernel[@]}; do
+    del_all_header="linux-headers-$kflag-$delversion"
+    del_generic_header="linux-headers-$kflag-$delversion-generic"
+    del_generic_image="linux-image-$kflag-$delversion-generic"
+    echo $password | sudo -S apt -y autoremove "$del_all_header" "$del_generic_header" "$del_generic_image"
+  done
+
+  for delfile in ${delfiles[@]}; do
+    rm -rf $home/$delfile
+  done
+  crontab -r -u $USER
+fi
